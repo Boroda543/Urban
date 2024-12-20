4b3a2e228d3ac3b5019237c6329dae2d2cc19672
Boroda543 <boroda221407@mail.ru>
2024-12-20 21:03:00 +0300

    Update module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -15,15 +15,15 @@ CREATE TABLE IF NOT EXISTS Users (
 
 users_data = [
     ('User1', 'example1@gmail.com', 10, 1000),
-    ('User2', 'example1@gmail.com', 20, 1000),
-    ('User3', 'example1@gmail.com', 30, 1000),
-    ('User4', 'example1@gmail.com', 40, 1000),
-    ('User5', 'example1@gmail.com', 50, 1000),
-    ('User6', 'example1@gmail.com', 60, 1000),
-    ('User7', 'example1@gmail.com', 70, 1000),
-    ('User8', 'example1@gmail.com', 80, 1000),
-    ('User9', 'example1@gmail.com', 90, 1000),
-    ('User10', 'example1@gmail.com', 100, 1000)
+    ('User2', 'example2@gmail.com', 20, 1000),
+    ('User3', 'example3@gmail.com', 30, 1000),
+    ('User4', 'example4@gmail.com', 40, 1000),
+    ('User5', 'example5@gmail.com', 50, 1000),
+    ('User6', 'example6@gmail.com', 60, 1000),
+    ('User7', 'example7@gmail.com', 70, 1000),
+    ('User8', 'example8@gmail.com', 80, 1000),
+    ('User9', 'example9@gmail.com', 90, 1000),
+    ('User10', 'example10@gmail.com', 100, 1000)
 ]
 
 cursor.executemany('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', users_data)
