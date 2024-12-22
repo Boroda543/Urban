9946abe082e5b57037679e7abc84c1ad2ab6d7b8
Boroda543 <boroda221407@mail.ru>
2024-12-22 13:03:24 +0300

    Update module_14_2.py

--- module_14_2.py
+++ module_14_2.py
@@ -1,7 +1,9 @@
 import sqlite3
 
-connection = sqlite3.connect('not_telegram.bd')
+connection = sqlite3.connect('not_telegram.db')
 cursor = connection.cursor()
+
+cursor.execute('DELETE FROM Users WHERE id = 6')
 
 cursor.execute('SELECT COUNT(*) FROM Users')
 total_users = cursor.fetchone()[0]
