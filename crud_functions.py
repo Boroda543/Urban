7ce49fb909d10bbdd79c235628f341b39f8b6714
Boroda543 <boroda221407@mail.ru>
2024-12-25 23:09:55 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -2,39 +2,24 @@ import sqlite3
 
 
 def initiate_db():
-    conn = sqlite3.connect('your_database.db')
+    conn = sqlite3.connect('products.db')
     cursor = conn.cursor()
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL UNIQUE,
-        email TEXT NOT NULL,
-        age INTEGER NOT NULL,
-        balance INTEGER NOT NULL DEFAULT 1000
+    CREATE TABLE IF NOT EXISTS Products (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        price INTEGER NOT NULL
     )
     ''')
     conn.commit()
     conn.close()
 
 
-def add_user(username, email, age):
-    conn = sqlite3.connect('your_database.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)
-    ''', (username, email, age, 1000))
-    conn.commit()
-    conn.close()
-
-
-def is_included(username):
-    conn = sqlite3.connect('your_database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM Users WHERE username = ?', (username,))
-    user = cursor.fetchone()
-    conn.close()
-    return user is not None
-
-
 def get_all_products():
-    return None
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Products')
+    products = cursor.fetchall()
+    conn.close()
+    return products
